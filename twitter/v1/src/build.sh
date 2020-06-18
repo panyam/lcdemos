90560aab712aea5fa6fbd2a281845e3ba4fd453c
Sriram Panyam <sri.panyam@gmail.com>
2020-06-17 22:57:11 -0700

    Moving to makefiles as single point of entry

--- twitter/v1/src/build.sh
+++
@@ -1,4 +0,0 @@
-OUT_DIR="./"
-
-python -m grpc_tools.protoc -I./protos  --python_out=$OUT_DIR --grpc_python_out=$OUT_DIR ./protos/*.proto
-python -m grpc_tools.protoc -I./protos  --python_out=./ --grpc_python_out=./ ./protos/google/api/*.proto
